2fdf3755d82beeac65d9e5ec4dabf91ac9bf350d
Stanislaw Jastrzab <nhqg73@motorolasolutions.com>
2021-01-20 13:03:30 +0100

    Add test insert

--- EFCoreTraining-Console/Program.cs
+++ EFCoreTraining-Console/Program.cs
@@ -22,7 +22,27 @@ namespace EFCoreTraining
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //InsertFromFile();
+            Insert();
+        }
+
+        private static void Insert()
+        {
+            Console.WriteLine("Inserting");
+            var factory = new ConsoleAppFactory();
+            var context = factory.CreateDbContext();
+            var postalCode = new PostalCode {Code = "Test code"};
+            var street = new Street {Name = "Test street", PostalCodes = new List<PostalCode> {postalCode}};
+            context.PostalCodes.Add(postalCode);
+            context.Streets.Add(street);
+            var result = context.SaveChanges();
+            Console.WriteLine("OK " + result);
+        }
+
+
+        private static void InsertFromFile()
+        {
+            Console.WriteLine("Inserting form file");
             List<GeoJson> items = new List<GeoJson>();
             using (StreamReader r = new StreamReader("C:\\pl_malopolskie-addresses-state.json"))
             {
@@ -30,8 +50,9 @@ namespace EFCoreTraining
                 while ((json = r.ReadLine()) != null)
                 {
                     items.Add(JsonConvert.DeserializeObject<GeoJson>(json));
-                }                 
+                }
             }
+
             var factory = new ConsoleAppFactory();
             var context = factory.CreateDbContext();
             var postalList = new List<PostalCode>();
@@ -41,18 +62,19 @@ namespace EFCoreTraining
             {
                 var postalCode = new PostalCode();
                 var street = new Street();
-                if (!postalList.Any(x => x.Code == item.properties.postcode))
+                if (postalList.All(x => x.Code != item.properties.postcode))
                 {
-                     postalCode = new PostalCode { Code = item.properties.postcode};
-                     postalList.Add(postalCode);
+                    postalCode = new PostalCode {Code = item.properties.postcode};
+                    postalList.Add(postalCode);
                 }
                 else
                 {
                     postalCode = postalList.First(x => x.Code == item.properties.postcode);
                 }
-                if (!streets.Any(x => x.Name == item.properties.street))
+
+                if (streets.All(x => x.Name != item.properties.street))
                 {
-                    street = new Street { Name = item.properties.street, PostalCodes = new List<PostalCode>()};
+                    street = new Street {Name = item.properties.street, PostalCodes = new List<PostalCode>()};
                     streets.Add(street);
                 }
                 else
@@ -61,9 +83,8 @@ namespace EFCoreTraining
                 }
 
                 street.PostalCodes.Add(postalCode);
-
-                
             }
+
             context.PostalCodes.AddRange(postalList);
             context.Streets.AddRange(streets);
             var result = context.SaveChanges();
